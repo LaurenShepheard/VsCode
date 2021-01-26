e053187390b362105b4e724d58ffaf7a9445d4ba
Lauren Shepheard <laurenshepheard@gmail.com>
2021-01-26 14:10:52 +0800

    poo

---
+++ Learningpython.py
@@ -0,0 +1,26 @@
+for i in range(2):
+    print("hello world")
+# I just learnt how to comment by putting the hash key at the start of a line. Also if I put a backslash after a command you can put the command on the next line.
+print\
+    ("""This is a long code,
+    it spreads over multiple lines,
+    because of the triple quotaions and brackets""")
+# A string is a sequence of one or more characters surrounded by quotes ' "", like above. It's data type is str.
+print('This is a String but if you are using numbers the data type is int and called integer')
+b = 100
+print(b)
+print(2+2)
+# Numbers with a decimal are called a float and act like ints. True and false are bool data type called booleans.
+print(2/2)
+print("The number above is a constant as its value does not change whereas the b is a variable as I assigned it a value using the assignment operator =, doing this you can do math")
+a = 50
+y = a + b
+print(y)
+a = a + 1
+# The above is an example of incrementing a variable, you can decrement it by using - as well. You can also skip putting the a like below.
+a += 1
+y = a + b
+print(y)
+print("Now the number changes because I incremented the variable.")
+Nick = "A really cool guy who is probably a jedi but who really knows"
+print(Nick)
